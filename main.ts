987008902638a0eca490defe59659f4a502a7147
marcinmoskala <marcinmoskala@gmail.com>
2022-07-18 15:07:27 +0200

    Update

--- main.ts
+++ main.ts
@@ -1,137 +1,159 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {addIcon, App, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {AnkiConnector} from 'anki-markdown';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	standardPrefix: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	standardPrefix: ''
 }
 
-export default class MyPlugin extends Plugin {
+export default class AnkiMarkdownPlugin extends Plugin {
 	settings: MyPluginSettings;
+	connector: AnkiConnector = new AnkiConnector();
+	actionsShown: boolean = false;
+	icons: HTMLElement[] = [];
 
 	async onload() {
-		await this.loadSettings();
+		this.addActionsIfAnkiConnected()
+		this.registerInterval(window.setInterval(() => this.addActionsIfAnkiConnected(), 10 * 1000));
+		// await this.loadSettings();
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	private async addActionsIfAnkiConnected() {
+		const connected = await this.connector.checkConnection()
+		if (connected && !this.actionsShown) {
+			this.addActions();
+			this.actionsShown = true;
+		}
+		if (!connected && this.actionsShown) {
+			this.icons.forEach(e => e.detach())
+			this.icons = []
+			this.actionsShown = false;
+		}
+	}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	private addActions() {
+		addIcon('push', `<path fill="currentColor" stroke="currentColor" d="M 27.00,3.53 C 18.43,6.28 16.05,10.38 16.00,19.00 16.00,19.00 16.00,80.00 16.00,80.00 16.00,82.44 15.87,85.73 16.74,88.00 20.66,98.22 32.23,97.00 41.00,97.00 41.00,97.00 69.00,97.00 69.00,97.00 76.63,96.99 82.81,95.84 86.35,88.00 88.64,82.94 88.00,72.79 88.00,67.00 88.00,67.00 88.00,24.00 88.00,24.00 87.99,16.51 87.72,10.42 80.98,5.65 76.04,2.15 69.73,3.00 64.00,3.00 64.00,3.00 27.00,3.53 27.00,3.53 Z M 68.89,15.71 C 74.04,15.96 71.96,19.20 74.01,22.68 74.01,22.68 76.72,25.74 76.72,25.74 80.91,30.85 74.53,31.03 71.92,34.29 70.70,35.81 70.05,38.73 67.81,39.09 65.64,39.43 63.83,37.03 61.83,36.00 59.14,34.63 56.30,35.24 55.08,33.40 53.56,31.11 56.11,28.55 56.20,25.00 56.24,23.28 55.32,20.97 56.20,19.35 57.67,16.66 60.89,18.51 64.00,17.71 64.00,17.71 68.89,15.71 68.89,15.71 Z M 43.06,43.86 C 49.81,45.71 48.65,51.49 53.21,53.94 56.13,55.51 59.53,53.51 62.94,54.44 64.83,54.96 66.30,56.05 66.54,58.11 67.10,62.74 60.87,66.31 60.69,71.00 60.57,74.03 64.97,81.26 61.40,83.96 57.63,86.82 51.36,80.81 47.00,82.22 43.96,83.20 40.23,88.11 36.11,87.55 29.79,86.71 33.95,77.99 32.40,74.18 30.78,70.20 24.67,68.95 23.17,64.97 22.34,62.79 23.39,61.30 25.15,60.09 28.29,57.92 32.74,58.49 35.44,55.57 39.11,51.60 36.60,45.74 43.06,43.86 Z" />`)
+		addIcon('pull', `<path fill="currentColor" stroke="currentColor" d="M 27.00,3.53 C 18.43,6.28 16.05,10.38 16.00,19.00 16.00,19.00 16.00,80.00 16.00,80.00 16.00,82.44 15.87,85.73 16.74,88.00 20.66,98.22 32.23,97.00 41.00,97.00 41.00,97.00 69.00,97.00 69.00,97.00 76.63,96.99 82.81,95.84 86.35,88.00 88.64,82.94 88.00,72.79 88.00,67.00 88.00,67.00 88.00,24.00 88.00,24.00 87.99,16.51 87.72,10.42 80.98,5.65 76.04,2.15 69.73,3.00 64.00,3.00 64.00,3.00 27.00,3.53 27.00,3.53 Z M 68.89,15.71 C 74.04,15.96 71.96,19.20 74.01,22.68 74.01,22.68 76.72,25.74 76.72,25.74 80.91,30.85 74.53,31.03 71.92,34.29 70.70,35.81 70.05,38.73 67.81,39.09 65.64,39.43 63.83,37.03 61.83,36.00 59.14,34.63 56.30,35.24 55.08,33.40 53.56,31.11 56.11,28.55 56.20,25.00 56.24,23.28 55.32,20.97 56.20,19.35 57.67,16.66 60.89,18.51 64.00,17.71 64.00,17.71 68.89,15.71 68.89,15.71 Z M 43.06,43.86 C 49.81,45.71 48.65,51.49 53.21,53.94 56.13,55.51 59.53,53.51 62.94,54.44 64.83,54.96 66.30,56.05 66.54,58.11 67.10,62.74 60.87,66.31 60.69,71.00 60.57,74.03 64.97,81.26 61.40,83.96 57.63,86.82 51.36,80.81 47.00,82.22 43.96,83.20 40.23,88.11 36.11,87.55 29.79,86.71 33.95,77.99 32.40,74.18 30.78,70.20 24.67,68.95 23.17,64.97 22.34,62.79 23.39,61.30 25.15,60.09 28.29,57.92 32.74,58.49 35.44,55.57 39.11,51.60 36.60,45.74 43.06,43.86 Z" />`)
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.icons.push(this.addRibbonIcon('push', 'Push to Anki', async (evt: MouseEvent) => {
+			const activeFile = this.app.workspace.getActiveFile()
+			if (!activeFile) return
+			try {
+				const content = await this.app.vault.read(activeFile);
+				const deckName = fileNameToDeckName(activeFile.name);
+				console.log(content)
+				console.log(deckName)
+				const result = await this.connector.pushDeck(deckName, content);
+				await this.app.vault.modify(activeFile, result.updatedMarkdown)
+				new Notice(`Success\nAdded: ${result.addedCount}\nRemoved: ${result.removedCount}\nUpdated: ${result.updatedCount}\nUnchanged: ${result.unchangedCount}`);
+			} catch (e) {
+				console.log(e)
+				new Notice(`Error while pushing cards ${e?.message}`);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+		}));
+
+		this.icons.push(this.addRibbonIcon('pull', 'Pull from Anki', async (evt: MouseEvent) => {
+			const activeFile = this.app.workspace.getActiveFile()
+			if (!activeFile) return
+			try {
+				const content = await this.app.vault.read(activeFile);
+				const deckName = fileNameToDeckName(activeFile.name);
+				const result = await this.connector.pullDeckToExisting(deckName, content);
+				await this.app.vault.modify(activeFile, result.updatedMarkdown)
+				new Notice(`Success`);
+			} catch (e) {
+				console.log(e)
+				new Notice(`Error while pushing cards ${e?.message}`);
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.icons.push(this.addRibbonIcon('plus', 'Add deck from Anki', async (evt: MouseEvent) => {
+			let names = await this.connector.getDeckNames()
+			names = names.filter(checked => !names.find(n => n != checked && n.startsWith(checked)))
+			new DecksModal(this.app, names, this.connector).open()
+			new Notice(`Success`);
+		}));
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	// async loadSettings() {
+	// 	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	// }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+// class SampleSettingTab extends PluginSettingTab {
+// 	plugin: MyPlugin;
+//
+// 	constructor(app: App, plugin: MyPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+//
+// 	display(): void {
+// 		const {containerEl} = this;
+//
+// 		containerEl.empty();
+//
+// 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+//
+// 		new Setting(containerEl)
+// 			.setName('Standard Prefix')
+// 			.setDesc('The prefix that should be used for all decks')
+// 			.addText(text => text
+// 				.setPlaceholder('')
+// 				.setValue(this.plugin.settings.standardPrefix)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.standardPrefix = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+// 	}
+// }
+
+class DecksModal extends Modal {
+	constructor(app: App, public names: Array<string>, public connector: AnkiConnector) {
 		super(app);
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.createEl('h3', {text: 'Add deck'})
+		const modalContainer = contentEl.createDiv({cls: 'anki-decks-container'});
+		this.names.map((deckName: string) => {
+			modalContainer.createEl("p")
+				.createEl("a", {text: deckName, cls: 'anki-decks-item'})
+				.addEventListener('click', async (e) => {
+					const data = await this.connector.pullDeck(deckName)
+					const fileName = deckNameToFileName(deckName)
+					const file = await this.app.vault.create(fileName, data.updatedMarkdown)
+					this.app.workspace.getLeaf().openFile(file);
+					this.close();
+				});
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		this.contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function deckNameToFileName(deckName: string): string {
+	return deckName.replace(/::/g, "__") + ".md"
+}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function fileNameToDeckName(deckName: string): string {
+	return deckName.replace(/__/g, "::")
+		.replace(".md", "")
 }
